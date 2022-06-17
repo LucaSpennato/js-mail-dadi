45790174ddf7144932ca4e34c3e7a47d591e28d0
LucaSpennato <luca.spennato@gmail.com>
2022-06-17 18:08:13 +0200

    math random 6, resulto on event button click

--- dadi/script.js
+++ dadi/script.js
@@ -1,12 +1,25 @@
-const user = parseInt((Math.floor(Math.random() * 10) + 1));
-const computer = parseInt((Math.floor(Math.random() * 10) + 1));
-
+const user = parseInt((Math.floor(Math.random() * 6) + 1));
+const computer = parseInt((Math.floor(Math.random() * 6) + 1));
 console.log(user, computer);
 
-if(user < computer){
-    console.log(`Il tuo numero: ${user} è più basso rispetto al numero del computer ${computer}, quindi hai perso! `);
-} else if (user === computer){
-    console.log(`Avete fatto entrambi ${user}, pareggio!`);
-} else{
-    console.log(`Il tuo numero ${user}, è più alto del computer: ${computer}, hai vinto!`);
-}
+let buttonPlay = document.getElementById('play');
+let result = document.getElementById('result');
+
+buttonPlay.addEventListener('click', function () {
+    if (user < computer) {
+        result.innerHTML = `Il tuo numero: ${user} è più basso rispetto al numero del computer ${computer}, quindi hai perso!`
+        console.log(`Il tuo numero: ${user} è più basso rispetto al numero del computer ${computer}, quindi hai perso! `);
+    } else if (user === computer) {
+        result.innerHTML = `Avete fatto entrambi ${user}, pareggio!`
+        console.log(`Avete fatto entrambi ${user}, pareggio!`);
+    } else {
+        result.innerHTML = `Il tuo numero ${user}, è più alto del computer: ${computer}, hai vinto!`
+        console.log(`Il tuo numero ${user}, è più alto del computer: ${computer}, hai vinto!`);
+    }
+
+})
+
+
+
+
+
